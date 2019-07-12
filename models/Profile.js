83a362613df4ab3a3f61011fee98acef0b2ba3ee
Kevin de Borst <kevin.de.borst@gmail.com>
2019-07-12 19:39:56 +0200

    Created Profile Model & DevTest Folder

---
+++ models/Profile.js
@@ -0,0 +1,87 @@
+//Required Dependencies
+const mongoose = require('mongoose');
+
+//Create Profile Schema
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  experience: [
+    {
+      event_type: {
+        type: String
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      cuisine_type: {
+        type: String
+      },
+      description: {
+        type: String
+      },
+      guest_amount: {
+        type: Number
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    pinterest: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
