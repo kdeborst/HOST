e7d8cbcf3c0aaf2b6465575ce6264258e45728b7
Kevin de Borst <kevin.de.borst@gmail.com>
2019-07-12 20:16:50 +0200

    Created "individual" profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,31 @@
 //Required Dependencies
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const authentication = require('../../middleware/authentication');
 
-//Route:        GET api/profile
-//Description:  Test Route
-//Access:       Public
-router.get('/', (req, res) => res.send('Profile route'));
+//Route:        GET api/profile/individual
+//Description:  Get personal profile of current user
+//Access:       Private
+router.get('/individual', authentication, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'We have found no profile for this user..' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
