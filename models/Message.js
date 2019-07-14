8e76c867d6307f94991a903d08e3c578dc4b2c7d
Kevin de Borst <kevin.de.borst@gmail.com>
2019-07-14 12:00:11 +0200

    Created Message Model

---
+++ models/Message.js
@@ -0,0 +1,57 @@
+//Required Dependencies
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Profile Schema
+const MessageSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('message', MessageSchema);
